b9ac3a045334a207b24510a4b21f1f80da16b66a
sazid-zero <sharif.sazid.3@gmail.com>
2025-07-03 16:49:30 +0600

    App added

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Dashboard from "./pages/Dashboard.tsx";
+
+
+function App() {
+    return (
+
+        <div className="App bg-gray-200 overflow-auto max-h-full">
+
+            <Dashboard/>
+        </div>
+    )
+
+}
+
+export default App
